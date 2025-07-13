15c4aa56432d2e05a2663a2a1119086a40cd4e50
Phoenix/HotaruBlaze <DeriousHD@gmail.com>
2025-07-13 11:17:56 +0100

    Implement meta tags

---
+++ src/components/Meta.jsx
@@ -0,0 +1,18 @@
+export default function Meta({ title, description, image, url = "https://fluttershub.com" }) {
+  return (
+    <>
+      <title>{title}</title>
+      <meta property="og:url" content={url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:secure_url" content={image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="theme-color" content="#00ff99" />
+    </>
+  );
+}

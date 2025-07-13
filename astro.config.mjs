060f7942af79caa41f6d37eb22eb216bf284c2a9
Phoenix/HotaruBlaze <DeriousHD@gmail.com>
2025-07-13 10:50:09 +0100

    Well thats stupid it has to be like this

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import node from '@astrojs/node';
 export default defineConfig({
   output: 'server',
   adapter: node({
-    host: '0.0.0.0',
-    port: 4321, // default port for Astro SSR
     mode: 'standalone', // bundles dependencies with output
   }),
   integrations: [react()],
